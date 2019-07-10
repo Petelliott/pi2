930982b9b9becdafa762ea454ef024255c56cca7
Peter Elliott <pelliott@ualberta.ca>
2019-07-09 21:11:55 -0700

    added len to rcstring

--- buffer/src/rcstring.rs
+++ buffer/src/rcstring.rs
@@ -1,4 +1,5 @@
 use std::rc::Rc;
+use std::cmp::min;
 
 #[derive(Clone)]
 pub struct RcString {
@@ -24,13 +25,21 @@ impl RcString {
     pub fn substr(&self, off: usize, len: usize) -> Self {
         RcString {
             base: self.base.clone(),
-            off: self.off + off,
-            len: len,
+            off: self.off + min(self.len, off),
+            len: if self.len >= off {
+                min(len, self.len - off)
+            } else {
+                0
+            },
         }
     }
 
     pub fn slice(&self, lo: usize, hi: usize) -> Self {
         self.substr(lo, hi-lo)
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
     }
 }
 
@@ -50,7 +59,11 @@ mod tests {
         assert_eq!(rs.str(), "abcdefg");
         let rs2 = rs.substr(2, 4);
         assert_eq!(rs2.str(), "cdef");
-        assert_eq!(rs2.substr(2, 1).str(), "e");
+        let rs3 = rs2.substr(2, 1);
+        assert_eq!(rs3.str(), "e");
+        assert_eq!(rs3.substr(1,0).str(), "");
+        assert_eq!(rs3.substr(0,10).str(), "e");
+        assert_eq!(rs3.substr(15,3).str(), "");
     }
 
     #[test]
@@ -59,6 +72,22 @@ mod tests {
         assert_eq!(rs.str(), "abcdefg");
         let rs2 = rs.slice(2, 6);
         assert_eq!(rs2.str(), "cdef");
-        assert_eq!(rs2.slice(2, 3).str(), "e");
+        let rs3 = rs2.slice(2, 3);
+        assert_eq!(rs3.str(), "e");
+        assert_eq!(rs3.slice(0,10).str(), "e");
+        assert_eq!(rs3.substr(15,18).str(), "");
+    }
+
+    #[test]
+    fn test_len() {
+        let rs = RcString::new(String::from("abcdefg"));
+        assert_eq!(rs.len(), 7);
+        let rs2 = rs.substr(2, 4);
+        assert_eq!(rs2.len(), 4);
+        let rs3 = rs2.substr(2, 1);
+        assert_eq!(rs3.len(), 1);
+        assert_eq!(rs3.substr(1,0).len(), 0);
+        assert_eq!(rs3.substr(0,10).len(), 1);
+        assert_eq!(rs3.substr(15,3).len(), 0);
     }
 }
