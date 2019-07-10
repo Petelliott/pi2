9347cd3fa5374394c7f870251804c272199984bc
Peter Elliott <pelliott@ualberta.ca>
2019-07-09 21:31:45 -0700

    added a bunch of rope functions

--- buffer/src/lib.rs
+++ buffer/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod rcstring;
 //pub mod bufferstorage;
 pub mod rope;
 
--- buffer/src/rope.rs
+++ buffer/src/rope.rs
@@ -2,7 +2,7 @@ use std::rc::Rc;
 use std::vec::Vec;
 use std::option::Option;
 use std::str::Chars;
-//use crate::bufferstorage::{ImmStore, StorageView};
+use crate::rcstring::RcString;
 
 #[derive(Clone)]
 pub struct Node {
@@ -15,41 +15,103 @@ pub struct Node {
 #[derive(Clone)]
 pub enum Rope {
     Node(Rc<Node>),
-    Leaf(Rc<String>),
+    Leaf(RcString),
 }
 
 impl Rope {
-    fn str_iter(&self) -> RopeIter<StrIter> {
+    pub fn concat(r1: &Self, r2: &Self) -> Self {
+        Rope::Node(Rc::new(Node {
+            leftn:   r1.len(),
+            leftnnl: r1.lenlines(),
+            left:    r1.clone(),
+            right:   r2.clone(),
+        }))
+    }
+
+    pub fn len(&self) -> usize {
+        match &self {
+            Rope::Node(nd) => nd.leftn + nd.right.len(),
+            Rope::Leaf(rcs) => rcs.len(),
+        }
+    }
+
+    pub fn lenlines(&self) -> usize {
+        match &self {
+            Rope::Node(nd) => nd.leftnnl + nd.right.lenlines(),
+            Rope::Leaf(rcs) => {
+                let mut i = 0;
+                for ch in rcs.str().chars() {
+                    if ch == '\n' {
+                        i += 1;
+                    }
+                }
+                i
+            }
+        }
+    }
+
+    pub fn char_substr(&self, idx: usize, n: usize) -> Self {
+        match &self {
+            Rope::Leaf(rcs) => Rope::Leaf(rcs.substr(idx, n)),
+            Rope::Node(nd) =>
+                if idx > nd.leftn {
+                    nd.right.char_substr(idx-nd.leftn, n)
+                } else if idx + n < nd.leftn {
+                    nd.left.char_substr(idx, n)
+                } else {
+                    Rope::concat(
+                        &nd.left.char_substr(idx, nd.leftn),
+                        &nd.right.char_substr(0, n - (nd.leftn - idx)))
+                },
+        }
+    }
+
+    pub fn str_iter(&self) -> RopeIter<StrIter> {
+        RopeIter {
+            stack:    vec![self],
+            curriter: None,
+        }
+    }
+
+    pub fn char_iter(&self) -> RopeIter<CharIter> {
+        RopeIter {
+            stack:    vec![self],
+            curriter: None,
+        }
+    }
+
+    /*
+    fn line_iter(&self) -> RopeIter<LineIter> {
         RopeIter{
             stack:    vec![self],
             curriter: None,
         }
     }
 
-    fn char_iter(&self) -> RopeIter<CharIter> {
-        RopeIter{
-            stack:    vec![self],
-            curriter: None,
-        }
-    }
+    fn line_at(&self) -> Rope;
+    */
+
+    /*
+    fn line_slice(&self, idx: usize, n: usize) -> Self;
+    */
 }
 
 pub trait LeafIter<'a>: Iterator {
-    fn from(s: &'a String) -> Self;
+    fn from(s: &'a RcString) -> Self;
 }
 
-struct StrIter<'a> {
-    s: Option<&'a String>,
+pub struct StrIter<'a> {
+    s: Option<&'a str>,
 }
 
 impl<'a> LeafIter<'a> for StrIter<'a> {
-    fn from(s: &'a String) -> Self {
-        StrIter{ s: Some(s) }
+    fn from(s: &'a RcString) -> Self {
+        StrIter{ s: Some(s.str()) }
     }
 }
 
 impl<'a> Iterator for StrIter<'a> {
-    type Item = &'a String;
+    type Item = &'a str;
 
     fn next(&mut self) -> Option<Self::Item> {
         let r = self.s;
@@ -58,13 +120,13 @@ impl<'a> Iterator for StrIter<'a> {
     }
 }
 
-struct CharIter<'a> {
+pub struct CharIter<'a> {
     it: Chars<'a>,
 }
 
 impl<'a> LeafIter<'a> for CharIter<'a> {
-    fn from(s: &'a String) -> Self {
-        CharIter{ it: s.chars()}
+    fn from(s: &'a RcString) -> Self {
+        CharIter{ it: s.str().chars()}
     }
 }
 
